ea56b5af68fdc6ef333efbb54f3bc71221eda401
yuris0230 <178619823+yuris0230@users.noreply.github.com>
2025-09-14 15:30:00 +0000

    [Update] Update books_controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,59 @@
 class BooksController < ApplicationController
+  # according to AI mentor
+  # find book id that came from url
+  # no need to write every line at [:show, :edit, :update, :destroy] like sample app
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+
   def index
     @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if book.save
+      flash[:notice] = "投稿に成功しました。" # flash message
+      redirect_to book_path(@book.id) # if true -> book id
+    else
+      flash.now[:alert] = "投稿失敗しました。"
+      render :new # if false -> same form and need to do again
+      # redirect_to new_book_path -> this is for redirect
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    # book = book.find(params[:id])
+    # book.update(book_params)
+    # redirect_to book_path(book.id)
+    # need flash message to tell user that user's edit was successfull
+    if @book.update(book_params)
+      flash[:notice] = "更新に成功しました。"
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:alert] = "更新に失敗しました。"
+      render :edit
+    end
+  end
+
+  def destroy
+    @book.destroy
+    flash[:notice] = "削除しました。"
+    redirect_to books_path # redirect to book_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body) # strong parameters permit
+  end
+
+  # before-action method
+  def set_book
+    @book = Book.find(params[:id])
   end
 end
